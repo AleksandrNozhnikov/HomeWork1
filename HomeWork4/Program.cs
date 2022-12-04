7733b7ce15c609eb2258362f04da7e40e3454bf3
Aleksandr <nozhnikov89@mail.ru>
2022-12-04 23:47:16 +0300

    add homework4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход ринимает число (N),
+// а на выходе показывает все четные числа от 1 до N.
+// 5 ->2, 4
+// 8 ->2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while((count) <= N)
+{
+        Console.WriteLine("{0} ", count);
+        count = count + 2;
+}
