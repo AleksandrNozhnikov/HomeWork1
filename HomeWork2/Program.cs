7b71926913f11e63bb30205e7a51d0d4b4247718
Aleksandr <nozhnikov89@mail.ru>
2022-12-04 16:54:25 +0300

    add homework1 final

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдает 
+// максимальное из этих чисел
+// 2,3,7 -> 7
+// 44,5,78 -> 78
+// 22,3,9 -> 22
+
+
+Console.Clear();
+Console.WriteLine("Введите число");
+Console.WriteLine("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a != b)
+{
+if (a < b)
+{
+    Console.WriteLine("a < b " + a + "<" + b);
+}
+else
+{
+    Console.WriteLine("a > b " + a + ">" + b);
+}
+    }
+else
+{
+    Console.WriteLine("a > b " + a + ">" + b);
+}
