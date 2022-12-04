892332a0b96cb9ab234b3bf648a868a75fead562
Aleksandr <nozhnikov89@mail.ru>
2022-12-04 22:25:14 +0300

    add homework3

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли чтсло четным (делится ли на два без остатка) 
+//  4 -> да
+// -3 -> нет
+//  7 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a % 2) !=0)
+{
+        Console.WriteLine("Число четное? Нет.");
+}
+
+else Console.WriteLine("Число четное? Да.");
